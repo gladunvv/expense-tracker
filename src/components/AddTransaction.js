66cd6e7a0931cd249a176f5dd0ac4a918de1e98c
vladislav <bincha.1997@gmail.com>
2020-08-17 23:41:08 +1000

    feat: update components

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -4,18 +4,19 @@ export const AddTransaction = () => {
   return (
     <>
       <h3>Add new transaction</h3>
-      <form id='form'>
+      <form>
         <div className='form-contol'>
           <label htmlFor='text'></label>
-          <input type='text' id='text' placeholder='Enter text...' />
+          <input type='text' placeholder='Enter text...' />
         </div>
         <div className='form-conrol'>
           <label htmlFor='amount'>
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type='number' id='amount' placeholder='Enter amount...' />
+          <input type='number' placeholder='Enter amount...' />
         </div>
+        <button className='btn'>Add transaction</button>
       </form>
     </>
   );
