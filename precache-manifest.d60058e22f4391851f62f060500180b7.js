05b5e42ec89e5512f8ee34a0080a62c89a30899a
vladislav <bincha.1997@gmail.com>
2020-08-22 00:34:14 +1000

    Updates

---
+++ precache-manifest.d60058e22f4391851f62f060500180b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a013bae05bff5a245d7f81b42da2baac",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "b145e064474bfd7680f1",
+    "url": "/expense-tracker/static/css/main.ee45eb7d.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b145e064474bfd7680f1",
+    "url": "/expense-tracker/static/js/main.cb709482.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
